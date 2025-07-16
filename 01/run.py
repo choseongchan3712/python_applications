4f7d8b3486447b3d560aa0497d95b0390d03a63c
choseongchan3712 <choseongchan3712@naver.com>
2025-07-16 22:35:10 +0900

    import

---
+++ 01/run.py
@@ -0,0 +1,12 @@
+import calculator as calc
+from calculator import add, multiply, divide, subtract
+
+print(calc.add(2, 5))
+# 7
+print(calc.multiply(3, 4))
+# 12
+
+print(add(2, 5))
+# 7
+print(multiply(3, 4))
+# 12
